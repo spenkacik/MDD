375517b0b37cf5614395964da8388ba24c1f4b61
Samuel Penkacik <spenkacik@fullsail.edu>
2013-04-17 19:09:33 -0400

    Basic API functionality addded

--- blog/fuel/app/views/entry/index.php
+++ blog/fuel/app/views/entry/index.php
@@ -10,6 +10,65 @@
 		</tr>
 	</thead>
 	<tbody>
+	<div id="fb-root"></div>
+<script>
+  // Additional JS functions here
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '184227598396934', // App ID
+      channelUrl : '//localhost', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional init code here
+    FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    // connected
+    testAPI();
+  } else if (response.status === 'not_authorized') {
+    // not_authorized
+    login();
+  } else {
+    // not_logged_in
+    login();
+  }
+ });
+
+  };
+  
+	  //prompt user to login if not already
+  function login() {
+    FB.login(function(response) {
+	        if (response.authResponse) {
+	            // connected
+	            window.fbAsyncInit();
+	        } else {
+	            // cancelled
+	        }
+	    });
+	}
+	
+	function testAPI() {
+	    console.log('Welcome!  Fetching your information from Facebook.... ');
+	    FB.api('/me', function(response) {
+	        console.log('Good to see you, ' + response.name + '.');
+	        document.getElementById('inner').innerHTML = 'Welcome, ' + response.name + '.';
+	    });
+	}
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+</script>
+
+<p id="inner"></p>
 <?php foreach ($entries as $entry): ?>		<tr>
 
 			<td><?php echo $entry->title; ?></td>
